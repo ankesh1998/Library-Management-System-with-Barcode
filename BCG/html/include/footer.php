03c5a8ce738f4a8c51d62c3fa76e102d036dc86e
Ankesh Pandey <ankeshpandey987@gmail.com>
2019-07-18 22:36:05 +0530

    Add files via upload

---
+++ BCG/html/include/footer.php
@@ -0,0 +1,26 @@
+<?php
+if (!defined('IN_CB')) { die('You are not allowed to access to this page.'); }
+?>
+
+            <div class="output">
+                <section class="output">
+                    <h3>Output</h3>
+                    <?php
+                        $finalRequest = '';
+                        foreach (getImageKeys() as $key => $value) {
+                            $finalRequest .= '&' . $key . '=' . urlencode($value);
+                        }
+                        if (strlen($finalRequest) > 0) {
+                            $finalRequest[0] = '?';
+                        }
+                    ?>
+                    <div id="imageOutput">
+                        <?php if ($imageKeys['text'] !== '') { ?><img src="image.php<?php echo $finalRequest; ?>" alt="Barcode Image" /><?php }
+                        else { ?>Fill the form to generate a barcode.<?php } ?>
+                    </div>
+                </section>
+            </div>
+        </form>
+
+    </body>
+</html>
